1ef7f049204e4bc5c8af476097f5eec88ab3f58e
BrodBoyChees <ost515132@gmail.com>
2022-03-21 22:27:13 +0100

    cum

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const Discord = require('discord.js')
 
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
+
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -19,5 +22,15 @@ client.on("messageCreate", (message) => {
     }
 })
 
+const welcomeChannelId = "955533793362587698"
 
-client.login(process.env.TOKENS)
+client.on("guildMemberAdd", async (member) => {
+    const img= await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> welcome to the penis boiiz!`,
+        files: [img]
+    })
+    
+})
+
+client.login(process.env.TOKEN)
